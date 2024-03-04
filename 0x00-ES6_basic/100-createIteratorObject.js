f4a935b5878e377958f57b8751926deb4971a051
mikehailu7 <mikehailu7@gmail.com>
2024-03-04 02:22:13 -0800

    ES6 basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,12 @@
-//Author: MikiasHailu
 export default function createIteratorObject(report) {
-	const emps = [];
-	// eslint-disable-next-line
-	for (const dep of Object.keys(report.allEmployees)) {
-		// eslint-disable-next-line
-		for (const emp of report.allEmployees[dep]) {
-			emps.push(emp);
-		}
-	}
+  const emps = [];
+  // eslint-disable-next-line
+  for (const dep of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+    for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
+    }
+  }
 
-	return emps;
+  return emps;
 }
