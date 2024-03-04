66df3d076c251d76b4771a88ac95f5fc62ddccb5
mikehailu7 <mikehailu7@gmail.com>
2024-03-04 02:25:42 -0800

    ES6 basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
-//Author: MikiasHailu
 export default function appendToEachArrayValue(array, appendString) {
-	// eslint-disable-next-line
-	for (const value of array) {
-		// eslint-disable-next-line
-		array[array.indexOf(value)] = appendString + value;
-	}
+  // eslint-disable-next-line
+  for (const value of array) {
+    // eslint-disable-next-line
+    array[array.indexOf(value)] = appendString + value;
+  }
 
-	return array;
+  return array;
 }
