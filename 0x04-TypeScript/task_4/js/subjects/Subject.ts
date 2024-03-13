34371b565621b07842ab698ca0bd0ebcf5ef8c65
mikehailu7 <mikehailu7@gmail.com>
2024-03-13 05:30:30 -0700

    task4

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,12 @@
+//Author: MikiasHailu
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+    export class Subject {
+      teacher: Subjects.Teacher;
+  
+      set setTeacher(teacher: Teacher) {
+        this.teacher = teacher;
+      }
+    }
+  }
